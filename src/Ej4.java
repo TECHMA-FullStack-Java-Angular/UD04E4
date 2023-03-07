229c3fa915ecf9a7e97c8f12f027d77b1ee7b6a9
Paula Sousa <paulasousa137@gmail.com>
2023-03-07 12:53:11 +0100

    Ej4 final

---
+++ src/Ej4.java
@@ -0,0 +1,24 @@
+
+public class Ej4 {
+	public static void main(String args[]) {
+		int n=1;
+		int contador=0;
+		while(contador<77) {
+			n++;
+			contador++;
+		}
+			System.out.println(n);
+		
+	
+	int contador2=0;
+	while(contador2<3) {
+		n--;
+		contador2++;
+	}
+		System.out.println(n);
+		System.out.println("Duplicar valor de n : "+(n*2));
+	}
+	
+	
+}
+
